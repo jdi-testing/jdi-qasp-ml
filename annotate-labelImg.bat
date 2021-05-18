ac763bc551c2293f08d518b52c5e5b4559c4d9dc
Vyacheslav Fuga <vyacheslav_fuga@epam.com>
2021-05-18 16:53:52 +0300

    alternative dataset

--- annotate-labelImg.bat
+++ annotate-labelImg.bat
@@ -1,1 +1,1 @@
-labelImg.exe D:\DataScience\!WORK\jdi-qasp-ml\dataset\images dataset\annotations\classes.txt
+labelImg.exe "D:\DataScience\!WORK\jdi-qasp-ml\dataset\images" "D:\DataScience\!WORK\jdi-qasp-ml\dataset\annotations\classes.txt"

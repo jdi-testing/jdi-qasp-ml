e2de93989781152f16ad6bee955e09212eb8a514
Vyacheslav Fuga <vyacheslav_fuga@epam.com>
2021-06-21 13:36:54 +0300

    is_hidden in main.py - fixed

--- run-docker.bat
+++ run-docker.bat
@@ -1,1 +1,2 @@
-docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml jdi-ml 
+@REM docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml jdi-ml 
+docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml registry.gitlab.com/vfuga/jdi-qasp-ml:latest

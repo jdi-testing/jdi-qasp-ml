53b28a85b0717c06d0fb591ae6c2f647ffa7420e
Vyacheslav Fuga <vyacheslav_fuga@epam.com>
2021-08-29 11:47:07 +0300

    denetate-data.bat added

---
+++ MUIgenerator/generate-data.bat
@@ -0,0 +1,303 @@
+set BUILD_PATH=../MUI_model/dataset/html/site-200
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-201
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-202
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-203
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-204
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-205
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-206
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-207
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-208
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-209
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-210
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-211
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-212
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-213
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-214
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-215
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-216
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-217
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-218
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-219
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-220
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-221
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-222
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-223
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-224
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-225
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-226
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-227
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-228
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-229
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-230
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-231
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-232
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-233
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-234
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-235
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-236
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-237
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-238
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-239
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-240
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-241
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-242
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-243
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-244
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-245
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-246
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-247
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-248
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-249
+call npm run generate
+
+set BUILD_PATH=../MUI_model/dataset/html/site-000
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-001
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-002
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-003
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-004
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-005
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-006
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-007
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-008
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-009
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-010
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-011
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-012
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-013
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-014
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-015
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-016
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-017
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-018
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-019
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-020
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-021
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-022
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-023
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-024
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-025
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-026
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-027
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-028
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-029
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-030
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-031
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-032
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-033
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-034
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-035
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-036
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-037
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-038
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-039
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-040
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-041
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-042
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-043
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-044
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-045
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-046
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-047
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-048
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-049
+call npm run generate
+
+set BUILD_PATH=../MUI_model/dataset/html/site-100
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-101
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-102
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-103
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-104
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-105
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-106
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-107
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-108
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-109
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-110
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-111
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-112
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-113
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-114
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-115
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-116
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-117
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-118
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-119
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-120
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-121
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-122
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-123
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-124
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-125
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-126
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-127
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-128
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-129
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-130
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-131
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-132
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-133
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-134
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-135
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-136
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-137
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-138
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-139
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-140
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-141
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-142
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-143
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-144
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-145
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-146
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-147
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-148
+call npm run generate
+set BUILD_PATH=../MUI_model/dataset/html/site-149
+call npm run generate
+

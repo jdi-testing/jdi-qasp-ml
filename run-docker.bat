e10b64b1bceb3e2062987d160529a6c4e7d45348
Nina Poliachek <nina_poliachek@epam.com>
2021-09-20 13:46:59 +0300

    Updates

--- run-docker.bat
+++ run-docker.bat
@@ -1,2 +1,2 @@
-@REM docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml jdi-ml 
-docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml registry.gitlab.com/vfuga/jdi-qasp-ml:latest
+docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml jdi-ml 
+@REM docker run -p 127.0.0.1:5000:5000/tcp -ti --rm --name jdi-ml registry.gitlab.com/vfuga/jdi-qasp-ml:latest

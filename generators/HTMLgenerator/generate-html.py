cbc961d2901346655409f9259655655598338d25
Semyon Aleksandrovskiy <psifisher@gmail.com>
2022-01-05 11:08:00 +0300

    HTML generator update

--- generators/HTMLgenerator/generate-html.py
+++ generators/HTMLgenerator/generate-html.py
@@ -2,8 +2,8 @@ import logging
 
 import argh
 
-from generators.HTMLgenerator.builders.html5_builder import HTML5Builder
-from generators.HTMLgenerator.builders.bootstrap_builder import BootstrapBuilder
+from builders.html5_builder import HTML5Builder
+from builders.bootstrap_builder import BootstrapBuilder
 
 import os
 import sys
@@ -36,7 +36,7 @@ def all(
 @argh.arg("--num_of_pages", default=10, help="Number of pages to generate")
 @argh.arg("--elements_on_page", default=40, help="Num of elements per page")
 def html5(
-    output_dir: str = "data/html5_dataset/generated",
+    output_dir: str = "dataset/generated",
     num_of_pages: int = 10,
     elements_on_page: int = 40,
 ):
@@ -49,12 +49,12 @@ def html5(
 
 
 @argh.arg(
-    "--output_dir", default="data/html5_dataset/generated", help="Output directory"
+    "--output_dir", default="dataset/generated", help="Output directory"
 )
 @argh.arg("--num_of_pages", default=10, help="Number of pages to generate")
 @argh.arg("--elements_on_page", default=40, help="Num of elements per page")
 def bootstrap(
-    output_dir: str = "data/html5_dataset/generated",
+    output_dir: str = "dataset/generated",
     num_of_pages: int = 10,
     elements_on_page: int = 40,
 ):

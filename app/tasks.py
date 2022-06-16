a769edcc181853c5e69f0c917c2f81ca3b91dd46
Fedor Spiridonov <fedor_spiridonov@epam.com>
2022-06-16 11:06:04 +0400

    create separate high_priority queue

--- app/tasks.py
+++ app/tasks.py
@@ -2,6 +2,11 @@ from app.celery_app import celery_app
 from utils.robula import generate_xpath
 
 
-@celery_app.task
+@celery_app.task(queue="default")
 def task_schedule_xpath_generation(element_id, document, config):
+    return generate_xpath(element_id, document, config)
+
+
+@celery_app.task(queue="high_priority")
+def task_schedule_xpath_generation_prioritized(element_id, document, config):
     return generate_xpath(element_id, document, config)

5e3016449555c7e0e99c16407f7734fd236b3cf6
Fedor Spiridonov <fedor_spiridonov@epam.com>
2022-06-22 10:59:45 +0400

    discard_prioiritization_commit

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,21 +1,6 @@
 from celery import Celery
-from kombu import Queue
 
 from app import REDIS_URL
 
-celery_app = Celery(
-    "app.main",
-    broker=REDIS_URL,
-    backend=REDIS_URL,
-    task_track_started=True,
-    task_default_queue="default",
-    task_create_missing_queues=False,
-    result_extended=True,
-    task_queues=(
-        Queue("default"),
-        Queue("high_priority"),
-        Queue("low_priority"),
-    ),
-)
-
-celery_app.autodiscover_tasks(["app.tasks"], force=True)
+celery_app = Celery("app.main", broker=REDIS_URL, backend=REDIS_URL, task_track_started=True)
+celery_app.autodiscover_tasks(['app.tasks'], force=True)

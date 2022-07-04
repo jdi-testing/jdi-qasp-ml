e0022b20892ce4b9f0fa0b0cd7fb5c9f0fe6c4d7
Fedor Spiridonov <fedor_spiridonov@epam.com>
2022-07-05 01:16:58 +0400

    add functionality to increase task's priority

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,10 +1,11 @@
 from celery import Celery
 
+import kombu_redis_priority.transport.redis_priority_async  # noqa
 from app import REDIS_URL
 
 celery_app = Celery(
     "app.main",
-    broker=REDIS_URL,
+    broker="redispriorityasync://redis:6379",
     backend=REDIS_URL,
     task_track_started=True,
     task_acks_late=True,

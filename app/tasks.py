fe505dbaeacf1f64821f8da0550d6be3fa48b1a2
Fedor Spiridonov <fedor_spiridonov@epam.com>
2022-08-25 17:31:52 +0400

    resolve merge requests with rc-1 commits

--- app/tasks.py
+++ app/tasks.py
@@ -1,11 +1,36 @@
+import datetime
+
+from lxml import etree, html
+
 import app.mail_backend as backends
 from app.celery_app import celery_app
+from app.redis_app import redis_app
 from utils.robula import generate_xpath
 
 
-@celery_app.task
-def task_schedule_xpath_generation(element_id, document_uuid, config):
-    return generate_xpath(element_id, document_uuid, config)
+@celery_app.task(bind=True)
+def task_schedule_xpath_generation(
+    self, element_id, document_uuid, config, session_id, website_url
+):
+    start_time = datetime.datetime.utcnow()
+    page = redis_app.get(document_uuid)
+    result = generate_xpath(element_id, page, config)
+    end_time = datetime.datetime.utcnow()
+    task_duration = end_time - start_time
+
+    document = html.fromstring(page)
+    element = document.xpath(element_id)[0]
+    tree = etree.ElementTree(document)
+    full_xpath = tree.getpath(element)
+    nesting_num = len(full_xpath.split("/")) - 1
+
+    task_kwargs = self.request.kwargs
+    task_kwargs["task_duration"] = str(task_duration)
+    task_kwargs["start_time"] = str(start_time)
+    task_kwargs["full_xpath"] = full_xpath
+    task_kwargs["nesting_num"] = nesting_num
+
+    return result
 
 
 @celery_app.task(

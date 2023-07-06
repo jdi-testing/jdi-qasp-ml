23da9e151e37f9df08249d1097a7f71ee562c2b7
Fedor Spiridonov <fedor_spiridonov@epam.com>
2023-07-06 12:21:51 +0400

    issue_1332-revert_checking_limit_of_cpus_for_celery

--- start_celery.sh
+++ start_celery.sh
@@ -2,4 +2,4 @@
 
 CORES_TO_USE=$(($(nproc)-2>0?$(nproc)-2:1))
 echo $CORES_TO_USE
-celery -A app.celery_app:celery_app worker -l info --concurrency 1
+celery -A app.celery_app:celery_app worker -l info --concurrency $CORES_TO_USE

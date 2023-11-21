1f410986111b6b270e48a3cdc29bafc74400f454
Ekaterina Zykova <Ekaterina_Zykova@epam.com>
2023-11-21 14:20:43 +0100

    fix selenium_app.py

--- app/selenium_app.py
+++ app/selenium_app.py
@@ -8,14 +8,25 @@ from app.logger import logger
 
 
 def get_element_id_to_is_displayed_mapping(page_content_str):
+    dom = str(page_content_str).encode('utf-8').decode('unicode_escape')
+
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(
-        executable_path="/usr/local/bin/chromedriver-linux64/chromedriver", options=chrome_options)
 
-    driver.execute_script("document.write(arguments[0]);", page_content_str)
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "118.0",
+        "selenoid:options": {
+            "enableVideo": False
+        }
+    }
+
+    driver = webdriver.Remote(
+        command_executor="http://jdi-qasp-ml-selenoid:4444/wd/hub",
+        desired_capabilities=capabilities, options=chrome_options)
+
+    driver.execute_script("document.body.insertAdjacentHTML('beforeend', arguments[0]);", dom)
     wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
@@ -24,18 +35,10 @@ def get_element_id_to_is_displayed_mapping(page_content_str):
     result = {}
 
     for element in all_elements:
-        element_id = element.get_attribute("jdn-hash")  # got this format \"0000000000000000000000000000\"
-        element_id = str(element_id).replace('\\"', '')  # need to remove \"
+        element_id = element.get_attribute("jdn-hash")
         is_shown = element.is_displayed()
         result[element_id] = is_shown
-        print(f"{element}: {is_shown} {element.get_attribute('name')}")
-
-    # for element in all_elements:
-    #     element_id = element.get_attribute("jdn-hash")  # got this format \"0000000000000000000000000000\"
-    #     element_id = str(element_id).replace('\\"', '')  # need to remove \"
-    #     is_shown = element.is_displayed()
-    #     result[element_id] = is_shown
-    #     logger.info(f"Element with jdn-hash {element_id} {'Visible' if is_shown else 'Invisible'}")
+        logger.info(f"Element with jdn-hash {element_id} {'Visible' if is_shown else 'Invisible'}")
 
     driver.quit()
     return result

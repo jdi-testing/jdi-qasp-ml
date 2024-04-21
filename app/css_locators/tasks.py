1163fdd162e39238c2e9d24359ebea58198a302a
Ivan Golikov <ivan_golikov@epam.com>
2024-04-21 22:36:43 +0200

    Storing calculated selectors in cache

--- app/css_locators/tasks.py
+++ app/css_locators/tasks.py
@@ -4,6 +4,7 @@ from typing import List, Dict
 
 from app.celery_app import celery_app
 from app.selenium_app import get_webdriver
+from app.redis_app import redis_app
 
 
 logger = logging.getLogger(__name__)
@@ -23,13 +24,44 @@ def _replace_error_messages(new_message: str):
     return decorator
 
 
+def _cache_calculations_results(func):
+    def wrapper(*args, **kwargs):
+        elements_ids = kwargs["elements_ids"]
+        redis_keys = [f"css-selector-{e}" for e in elements_ids]
+        cached_selectors = redis_app.mget(redis_keys)
+
+        result = []
+        not_cached_elements_ids = []
+
+        for element_id, selector in zip(elements_ids, cached_selectors):
+            if selector is None:
+                not_cached_elements_ids.append(element_id)
+            else:
+                logger.info(f"Using cached selector for element {element_id}")
+                result.append({"id": element_id, "result": selector.decode("utf-8")})
+
+        if not_cached_elements_ids:
+            new_kwargs = kwargs.copy()
+            new_kwargs["elements_ids"] = not_cached_elements_ids
+
+            new_results = func(*args, **new_kwargs)
+
+            result.extend(new_results)
+
+            for new_result in new_results:
+                redis_app.set(f"css-selector-{new_result['id']}", new_result["result"], ex=60*60*24)
+        return result
+
+    return wrapper
+
+
 @celery_app.task(bind=True)
 @_replace_error_messages("Error generating CSS locator")
+@_cache_calculations_results
 def task_schedule_css_selector_generation(
         self, document_path: str, elements_ids: List[str]
 ) -> List[Dict[str, str]]:
     driver = get_webdriver()
-
     driver.get(f"file:///html/{Path(document_path).name}")
 
     result = []

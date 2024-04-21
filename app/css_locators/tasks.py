ccaa35aaa35e3f8b9157c3f69316fe7931c8db06
Ivan Golikov <ivan_golikov@epam.com>
2024-04-21 21:24:06 +0200

    Moved ugly try/except block to the decorator

--- app/css_locators/tasks.py
+++ app/css_locators/tasks.py
@@ -12,31 +12,36 @@ logger = logging.getLogger(__name__)
 CSS_SELECTOR_GEN_TASK_PREFIX = "css-selectors-gen-"
 
 
+def _replace_error_messages(new_message: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as exc:
+                raise RuntimeError(new_message) from exc
+        return wrapper
+    return decorator
+
+
 @celery_app.task(bind=True)
+@_replace_error_messages("Error generating CSS locator")
 def task_schedule_css_selector_generation(
         self, document_path: str, elements_ids: List[str]
 ) -> List[Dict[str, str]]:
-    # noinspection PyBroadException
-    # We need this try/except block to avoid sending stacktraces to the frontend
-    # but still log them
-    try:
-        driver = get_webdriver()
+    driver = get_webdriver()
 
-        driver.get(f"file:///html/{Path(document_path).name}")
+    driver.get(f"file:///html/{Path(document_path).name}")
 
-        result = []
-        for element_id in elements_ids:
-            result.append({
-                "id": element_id,
-                "result": driver.execute_script(
-                    f"""
-                    el = document.querySelector('[jdn-hash="{element_id}"]');
-                    return generateSelectorByElement(el);
-                    """
-                ),
-            })
-    except Exception as exc:
-        logger.exception("Error generating CSS locator")
-        raise RuntimeError("Error generating CSS locator") from exc
+    result = []
+    for element_id in elements_ids:
+        result.append({
+            "id": element_id,
+            "result": driver.execute_script(
+                f"""
+                el = document.querySelector('[jdn-hash="{element_id}"]');
+                return generateSelectorByElement(el);
+                """
+            ),
+        })
 
     return result

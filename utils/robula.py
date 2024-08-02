dc1bab5fed70cfcb4293755646a5728bde3a1218
Bojana Todorovic <bojana_todorovic@epam.com>
2024-08-02 10:24:52 +0200

    Ignored attributes added in robula model

--- utils/robula.py
+++ utils/robula.py
@@ -158,6 +158,10 @@ class RobulaPlus:
         self.element = element
         self.document = document
 
+        if config["ignored_attributes"] is not None and len(config["ignored_attributes"]) != 0:
+            self.attribute_black_list.extend(config["ignored_attributes"])
+            self.attribute_black_list = list(dict.fromkeys(self.attribute_black_list))
+
     def check_for_time_limit(self, start_time):
         evaluation_time_in_seconds = (
             datetime.datetime.now() - start_time

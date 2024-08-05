2271b4e4ec3e5d6220ac7dd00f2af13324464999
Bojana Todorovic <bojana_todorovic@epam.com>
2024-08-05 09:10:46 +0200

    Convert attr black_list in set

--- utils/robula.py
+++ utils/robula.py
@@ -124,7 +124,7 @@ class RobulaPlus:
             "alt",
             "value",
         ]
-        self.attribute_black_list = [
+        self.attribute_black_list = {
             "href",
             "src",
             "onclick",
@@ -140,7 +140,7 @@ class RobulaPlus:
             "fill",
             "xmlns",
             "data-label",
-        ]
+        }
         self.forbidden_tags = ["svg", "rect"]
 
         self.maximum_generation_time_in_seconds = config[
@@ -159,9 +159,7 @@ class RobulaPlus:
         self.document = document
 
         if config.ignored_attributes:
-            self.attribute_black_list.extend(config.ignored_attributes)
-            # Remove duplicates from the list
-            self.attribute_black_list = list(set(self.attribute_black_list))
+            self.attribute_black_list.update(config.ignored_attributes)
 
     def check_for_time_limit(self, start_time):
         evaluation_time_in_seconds = (

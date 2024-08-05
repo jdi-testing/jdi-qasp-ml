26bcd454821e9c51675c6267ce6bdb76b436d8aa
Bojana Todorovic <bojana_todorovic@epam.com>
2024-08-05 13:35:11 +0200

    Condition changed

--- utils/robula.py
+++ utils/robula.py
@@ -158,8 +158,8 @@ class RobulaPlus:
         self.element = element
         self.document = document
 
-        if config.ignored_attributes:
-            self.attribute_black_list.update(config.ignored_attributes)
+        if config["ignored_attributes"] is not None and len(config["ignored_attributes"]) != 0:
+            self.attribute_black_list.update(config["ignored_attributes"])
 
     def check_for_time_limit(self, start_time):
         evaluation_time_in_seconds = (
